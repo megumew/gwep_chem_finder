1499a84d9e479f1a4327cc9c07b5f145f3674b30
megumew <megumeww@gmail.com>
2022-09-16 22:00:21 -0400

    Moved the chemical structs to their own workspace to allow other modules to access the data types

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,71 +1,16 @@
+use std::fmt;
+
+use data::chemicals;
 use dm_pest::parser;
 // use dm_reader;
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-static BASES: [Base; 30] = [
-    Base { id: "aluminium" },
-    Base { id: "barium" },
-    Base { id: "bromine" },
-    Base { id: "calcium" },
-    Base { id: "carbon" },
-    Base { id: "chlorine" },
-    Base { id: "chromium" },
-    Base { id: "copper" },
-    Base { id: "ethanol" },
-    Base { id: "fluorine" },
-    Base { id: "hydrogen" },
-    Base { id: "iodine" },
-    Base { id: "iron" },
-    Base { id: "lithium" },
-    Base { id: "magnesium" },
-    Base { id: "mercury" },
-    Base { id: "nickel" },
-    Base { id: "nitrogen" },
-    Base { id: "oxygen" },
-    Base { id: "phosphorus" },
-    Base { id: "plasma" },
-    Base { id: "platinum" },
-    Base { id: "potassium" },
-    Base { id: "radium" },
-    Base { id: "silicon" },
-    Base { id: "silver" },
-    Base { id: "sodium" },
-    Base { id: "sugar" },
-    Base { id: "sulfur" },
-    Base { id: "water" },
-];
-
-struct Reagent {
-    name: Chemical,
-    quantity: u32,
-}
-
-enum Chemical {
-    Base(Base),
-    Compound(Compound),
-}
-
-#[derive(Debug)]
-struct Base {
-    id: &'static str,
-}
-
-struct Compound {
-    internal_name: String,
-    name: String,
-    id: String,
-    result: String,
-    def_start: usize,
-    def_end: usize,
-    required_reagents: Vec<Reagent>,
-    result_amount: u8,
-}
-
 fn main() {
     println!("Welcome to gwep chem finder!");
-    println!("Available bases: {:?}", BASES);
+    println!("Available Bases: {:?}", chemicals::BASES);
+
     // let result = dm_reader::dm_reader::read_file(String::from("recipes.DM"));
-    parser::read_file(String::from("recipes.DM"));
+    let file = parser::read_file(String::from("recipes.DM"));
 }
--- dm_pest/src/parser.rs
+++ dm_pest/src/parser.rs
@@ -1,3 +1,4 @@
+use data::chemicals::Reagent;
 use pest::Parser;
 use pest_derive::Parser;
 use std::fs;
@@ -9,8 +10,40 @@ pub struct DMParser;
 pub fn read_file(path: String) {
     let unparsed_file = fs::read_to_string(path).expect("cannot read file");
 
-    let file = DMParser::parse(Rule::file, &unparsed_file)
-        .expect("unsuccessful parse") // unwrap the parse result
-        .next()
-        .unwrap(); // get and unwrap the `file` rule; never fails
+    // let pairs = DMParser::parse(Rule::file, &unparsed_file)
+    //     .expect("unsuccessful parse") // unwrap the parse result
+    //     .next()
+    //     .unwrap(); // get and unwrap the `file` rule; never fails
+
+    let pairs = DMParser::parse(Rule::file, &unparsed_file).unwrap_or_else(|e| panic!("{}", e));
+
+    for pair in pairs {
+        // A pair is a combination of the rule which matched and a span of input
+        println!("Rule:    {:?}", pair.as_rule());
+        println!("Span:    {:?}", pair.as_span());
+        println!("Text:    {}", pair.as_str());
+
+        // A pair can be converted to an iterator of the tokens which make it up:
+        for inner_pair in pair.into_inner() {
+            match inner_pair.as_rule() {
+                Rule::obj => {
+                    let internal_name: String;
+                    let name: String;
+                    let id: String;
+                    let result: String;
+                    let mix_phrase: String;
+                    let required_reagents: Vec<Reagent>;
+                    let result_amount: u8;
+                    let hidden: Option<bool>;
+                    //println!("Compound:  {}", inner_pair.as_str());
+                    for field in inner_pair.into_inner() {
+                        println!("field: {}", field.as_str())
+                    }
+                    println!();
+                }
+                //Rule::digit => println!("Digit:   {}", inner_pair.as_str()),
+                _ => {}
+            };
+        }
+    }
 }
