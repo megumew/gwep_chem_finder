b7e575e26edd6667ecfb0f322e0a8a670e512a98
Quwin <ethantran.et77@gmail.com>
2022-10-19 04:54:44 -0700

    Searching by `name` field now implemented
    - Fixed issue occuring with Reactions with special characters such as `ñ`
    - Implemented searching by `name` field, expanding width of searches
    - Altered `insert_keyword()` and `string_permutations()` to be private functions
    - Altered `string_permutations()` to not redundantly include the first word of multi-word recipes twice
    - Altered `string_permutations()` to not include every character for every word, that's `insert_keyword()`'s job

--- data/src/search_engine.rs
+++ data/src/search_engine.rs
@@ -4,11 +4,11 @@ use crate::chemicals::Reaction;
 pub fn generate_search_keys(mut map: HashMap<String, Vec<String>>, reaction: Reaction) -> HashMap<String, Vec<String>> {
     let internal_name = reaction.get_internal_name();
     let result = reaction.get_result().to_lowercase();
-    // let name = reaction.get_name().to_lowercase();
+    let name = reaction.get_name().to_lowercase();
     let mut all_keywords: Vec<String> = Vec::new();
     all_keywords.append(&mut string_permutations(internal_name.clone()));
     all_keywords.append(&mut string_permutations(result));
-    // all_keywords.append(&mut string_permutations(name));
+    all_keywords.append(&mut string_permutations(name));
 
     for keyword in all_keywords {
         map = insert_keyword(map, keyword, &internal_name);
@@ -17,22 +17,24 @@ pub fn generate_search_keys(mut map: HashMap<String, Vec<String>>, reaction: Rea
     map
 }
 
-pub fn insert_keyword(mut map: HashMap<String, Vec<String>>, word: String, internal_name: &String) -> HashMap<String, Vec<String>> {
+fn insert_keyword(mut map: HashMap<String, Vec<String>>, word: String, internal_name: &String) -> HashMap<String, Vec<String>> {
     for k in 0..word.len() {
-        match map.get(&word[0..k + 1].to_string()) {
+        let chars = word.chars();
+        let string: String = chars.take(k+1).collect();
+        match map.get(&string) {
             Some(array) => {
                 if array.contains(internal_name) {
                     continue
                 } else {
                     map
-                        .entry(word[0..k + 1].to_string())
+                        .entry(string)
                         .or_default()
                         .push(internal_name.to_string());
                 }
             },
             None =>  {
                 map
-                    .entry(word[0..k + 1].to_string())
+                    .entry(string)
                     .or_default()
                     .push(internal_name.to_string());
             }
@@ -41,21 +43,35 @@ pub fn insert_keyword(mut map: HashMap<String, Vec<String>>, word: String, inter
     map
 }
 
-pub fn string_permutations(string: String) -> Vec<String> {
+fn string_permutations(string: String) -> Vec<String> {
     let mut permmutations: Vec<String> = Vec::new();
     permmutations.push(string.clone());
     if string.clone().contains("_") {
         permmutations.push(string.replace("_", ""));
         permmutations.push(string.replace("_", " "));
-        for word in string.split("_") {
-            permmutations.push(word.to_string());
+        let mut no_underscores = string.split("_");
+        no_underscores.next(); // The first word is covered by other permutations
+        loop {
+            let word = no_underscores.next();
+            if word == None {
+                break
+            }
+            permmutations.push(word.unwrap().to_string());
         }
     }
     if string.clone().contains(" ") {
         permmutations.push(string.replace(" ", ""));
         permmutations.push(string.replace(" ", "_"));
-        for word in string.split(" ") {
-            permmutations.push(word.to_string());
+        // Using `string.split_whitespace()` vs `string.split(" ") prevents potential issues with multiple spaces
+        let mut no_whitespace = string.split_whitespace();
+        no_whitespace.next(); // The first word is covered by other permutations
+        loop {
+            let word = no_whitespace.next();
+            if word == None {
+                return permmutations
+            }
+            println!("{}", word.unwrap());
+            permmutations.push(word.unwrap().to_string());
         }
     }
     permmutations
